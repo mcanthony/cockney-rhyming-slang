da625d7a3ffa7d3b45612d745356438820a553b3
Cole Willsea <coleww@gmail.com>
2015-08-11 08:08:09 -0700

    successfully parsed like almost 1k phrases

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-module.exports = function (str) {
-  return 'hello ' + str
-}
+var s = require('stopwords').english
+
+var phrases = require('./phrases')
+
+var g = phrases.filter(function(phrase){
+  var good = phrase.phrase.split(" ").filter(function(word){
+    return s.indexOf(word.toLowerCase()) === -1
+  })
+  phrase.words = good
+  return good.length === 2
+})
+
+console.log(g)
