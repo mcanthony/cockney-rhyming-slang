549bc10ac561af8b7204cbbcc86e1fbc3eab360c
Cole Willsea <coleww@gmail.com>
2015-08-12 08:01:24 -0700

    standardized and tested appropriately

--- index.js
+++ index.js
@@ -1,30 +1,28 @@
 var pronouncing = require('pronouncing')
 var phrases = require('./phrases')
 
-var the_burden = phrases.reduce(function(truth, p){ // yeah, burden of proof, the_truth!
+var the_burden = phrases.reduce(function (truth, p) { // yeah, burden of proof, the_truth!
   assignIt(truth, p)
   return truth
 }, {})
 
 function assignIt (obj, phr) {
-  if(!obj[phr.words[0]]) obj[phr.words[0]] = []
-  if(!obj[phr.words[1]]) obj[phr.words[1]] = []
+  if (!obj[phr.words[0]]) obj[phr.words[0]] = []
+  if (!obj[phr.words[1]]) obj[phr.words[1]] = []
 
-  obj[phr.words[1]].push({word: phr.words[0], other: phr.words[1], phrase: phr.phrase})
-  obj[phr.words[0]].push({word: phr.words[1], other: phr.words[0], phrase: phr.phrase})
+  obj[phr.words[1]].push({word: phr.words[0], rhyme: phr.words[1], phrase: phr.phrase})
+  obj[phr.words[0]].push({word: phr.words[1], rhyme: phr.words[0], phrase: phr.phrase})
 }
 
 module.exports = function (word) {
   var nickels = pronouncing.rhymes(word) // you know, like nickels and dimes, rhymes?
   if (!nickels.length) {
-    console.log('bonk, no rhymes', word)
     return false
   }
-  var donuts = nickels.filter(function(nickel){ // you know, like donut batches, matches? (ok this one is a stretch)
+  var donuts = nickels.filter(function (nickel) { // you know, like donut batches, matches? (ok this one is a stretch)
     return !!the_burden[nickel]
   })
   if (!donuts.length) {
-    console.log('bonk, no matches', word)
     return false
   }
 
--- test.js
+++ test.js
@@ -1,20 +1,18 @@
 var tap = require('tape')
 var cockneyRhymingSlang = require('./')
 
-// wow its gonna be rly hard to find repeatable test cases...
-
 tap.test('does the thing', function (t) {
   t.plan(1)
-  t.deepEqual(cockneyRhymingSlang('herd'), {word: 'hand', other: 'bird', phrase: 'bird in the hand'}, 'does it')
+  t.deepEqual(cockneyRhymingSlang('understand'), {rhyme: 'land', phrase: 'Nod land of', word: 'Nod'}, 'does it')
 })
 
 tap.test('if no words rhyme it returns undefined', function (t) {
   t.plan(1)
-  t.ok(!cockneyRhymingSlang('orange'), 'yup, good enough')
+  t.ok(!cockneyRhymingSlang('Christmas'), 'yup, good enough')
 })
 
 tap.test('if no rhyming words in the thing returns undefined', function (t) {
   t.plan(1)
-  t.ok(!cockneyRhymingSlang('whaaaaaaaaaaaaatgonna be hard to find a test case for this, yup. throw an error here specifically and HOPE HOPE or like run a dict/words through it, you know, whatever'), 'yup, good enough')
+  t.ok(!cockneyRhymingSlang('appropriate'), 'yup, good enough')
 })
 
