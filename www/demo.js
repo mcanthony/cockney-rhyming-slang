6d826c10dd86455a66bdebdc86c65ad350d0d93b
Cole Willsea <coleww@gmail.com>
2015-10-12 06:32:46 -0700

    nice

--- www/demo.js
+++ www/demo.js
@@ -2,5 +2,5 @@ var cockneyRhymingSlang = require('../')
 
 document.getElementById('input').addEventListener('keyup', function (e) {
   var thing = cockneyRhymingSlang(document.getElementById('input').value)
-  if (thing) document.getElementById('output').textContent = thing
+  if (thing) document.getElementById('output').textContent = JSON.stringify(thing)
 })
